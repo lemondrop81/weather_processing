7cddb62e02a308645f76d747b64e8657ae39e4a1
lemondrop81 <hacker814@hotmail.com>
2022-03-23 16:09:02 -0500

    Requests the URL

--- scrape_weather.py
+++ scrape_weather.py
@@ -13,3 +13,6 @@ class WeatherScraper(HTMLParser):
     def __init__(self):
         """Initialize the HTML Parser and initializes the variables."""
         HTMLParser.__init__(self)
+
+with urllib.request.urlopen('https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5') as response:
+    html = str(response.read())
