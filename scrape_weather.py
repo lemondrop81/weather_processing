e92e02e4148b9384f79dfa4897b3840689500eaa
lemondrop81 <hacker814@hotmail.com>
2022-03-23 16:49:18 -0500

    Check for the strong tag

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,6 +1,5 @@
 from html.parser import HTMLParser
 from html.entities import name2codepoint
-from typing import Counter
 import urllib.request
 
 """
@@ -18,6 +17,7 @@ class WeatherScraper(HTMLParser):
         self.tdTag = False
         self.trTag = False
         self.aTag = False
+        self.strongTag = False
         self.counter = 0
 
     def handle_starttag(self, tag, attrs):
@@ -30,6 +30,8 @@ class WeatherScraper(HTMLParser):
             self.tdTag = True
         if(tag == 'a'):
             self.aTag = True
+        if(tag == 'strong'):
+            self.strongTag = True
 
     def handle_endtag(self, tag):
         """Checks which end tag gets closed."""
@@ -42,10 +44,12 @@ class WeatherScraper(HTMLParser):
             self.tdTag = False
         if(tag == 'a'):
             self.aTag = False
+        if(tag == 'strong'):
+            self.strongTag = False
 
     def handle_data(self, data):
         """Handles the data inbetween the tags and adds it to a dictionary"""
-        if self.trTag == True and self.tbodyTag == True and self.tdTag == True and self.aTag == False and self.counter < 3:
+        if self.trTag == True and self.tbodyTag == True and self.tdTag == True and self.aTag == False and self.counter < 3 and self.strongTag == False:
             print(data)
             self.counter = self.counter + 1
         
