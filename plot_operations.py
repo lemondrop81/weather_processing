294b98a47d8752d0ccd8dc2a8f4640808d0e79c7
lemondrop81 <hacker814@hotmail.com>
2022-04-08 08:37:48 -0500

    create plot_operations file

---
+++ plot_operations.py
@@ -0,0 +1,12 @@
+import sqlite3
+import dbcm
+import matplotlib.pyplot as plt
+
+"""
+    Weather processing app
+    April 8, 2022
+    Description: A simple program to add to the database.
+"""
+
+class PlotOperations():
+    """Contains the code for plotting"""
