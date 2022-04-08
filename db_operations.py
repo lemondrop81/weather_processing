13385762d9277422abbcc35c35b8f7efef0777a7
lemondrop81 <hacker814@hotmail.com>
2022-04-08 07:58:13 -0500

    Update db_operations to use context manager

--- db_operations.py
+++ db_operations.py
@@ -10,33 +10,75 @@ import dbcm
 class DBOperations():
     """Contains database operations"""
 
-    def initialize_db(self):
-        """Initialize the database"""
-        dbcm.DBCM.initialize(self)
+    def __init__(self):
+        """Constructor"""
+        self.cursor = 0
+
+    def initialize(self):
+        """Initialize the database and create the table"""
+        try:
+            with dbcm.DBCM("weather.sqlite") as conn:
+                self.cursor = conn.cursor()
+                print("Opened database successfully.")
+                try:    
+                    c = self.cursor()
+                    c.execute("""create table IF NOT EXISTS weather 
+                                    (id integer primary key autoincrement,
+                                    sample_date text key not null,
+                                    location text,
+                                    min_temp real,
+                                    max_temp real,
+                                    avg_temp real);""")
+                    c.commit()
+                    print("Table created successfully.")
+                except Exception as e:
+                    print("Error creating table:", e)
+
+        except Exception as e:
+            print("Error opening DB:", e)
 
     def save_data(self, weather):
         """Save the data to the database"""
         try:
-            dbcm.DBCM.add_data(self, weather)
-
-            print("Data successfully saved.")
+            conn = sqlite3.connect("weather.sqlite")
+            c = conn.cursor()
+            sql = """insert OR REPLACE into weather (sample_date,location,min_temp,max_temp,avg_temp)
+                    values (?,?,?,?,?)"""
+            for k, v in weather.items():
+                data = (k, 'Winnipeg, MB', v['Min'], v['Max'], v['Mean'])
+                c.execute(sql, data)
+                print(data)
+            conn.commit()
+            print("Added sample successfully.")
         except Exception as e:
-            print("Error saving data:", e)
+            print("Error inserting sample.", e)
+
 
     def purge_data(self):
         """Purges all data from database"""
         try:
-            dbcm.DBCM.remove_data(self)
-
-            print("Data successfully removed.")
+            conn = sqlite3.connect("weather.sqlite")
+            c = conn.cursor()
+            c.execute("DELETE FROM weather")
+            conn.commit()
+            c.execute("DROP TABLE weather")
+            conn.commit()
         except Exception as e:
-            print("Error removing data:", e)
+            print("Error removing data.", e)
+        conn.close()
 
     def print(self):
         """Prints the data from database"""
-        dbcm.DBCM.print_data(self)
+        try:
+            conn = sqlite3.connect("weather.sqlite")
+            c = conn.cursor()
+            for row in c.execute("select * from weather"):
+                print(row)
+        except Exception as e:
+            print("Error fetching samples.", e)
+        conn.close()
             
 
 myparser = DBOperations()
 
-myparser.purge_data()
+myparser.initialize()
--- dbcm.py
+++ dbcm.py
@@ -8,63 +8,19 @@ import sqlite3
 class DBCM():
     """Contains database operations"""
 
-    def initialize(self):
-        """Initialize the database and create the table"""
-        try:
-            conn = sqlite3.connect("weather.sqlite")
-            print("Opened database successfully.")
-        except Exception as e:
-            print("Error opening DB:", e)
-        try:    
-            c = conn.cursor()
-            c.execute("""create table IF NOT EXISTS weather 
-                            (id integer primary key autoincrement,
-                            sample_date text key not null,
-                            location text,
-                            min_temp real,
-                            max_temp real,
-                            avg_temp real);""")
-            conn.commit()
-            print("Table created successfully.")
-        except Exception as e:
-            print("Error creating table:", e)
-        conn.close()
+    def __init__(self, db_name):
+        """Constructor"""
+        self.db_name = db_name
 
-    def add_data(self, weather):
-        """Adds the weather to the database"""
-        try:
-            conn = sqlite3.connect("weather.sqlite")
-            c = conn.cursor()
-            sql = """insert OR REPLACE into weather (sample_date,location,min_temp,max_temp,avg_temp)
-                    values (?,?,?,?,?)"""
-            for k, v in weather.items():
-                data = (k, 'Winnipeg, MB', v['Min'], v['Max'], v['Mean'])
-                c.execute(sql, data)
-                print(data)
-            conn.commit()
-            print("Added sample successfully.")
-        except Exception as e:
-            print("Error inserting sample.", e)
+    def __enter__(self):
+        """
+        Open the database connection
+        """
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
 
-    def print_data(self):
-        try:
-            conn = sqlite3.connect("weather.sqlite")
-            c = conn.cursor()
-            for row in c.execute("select * from weather"):
-                print(row)
-        except Exception as e:
-            print("Error fetching samples.", e)
-        conn.close()
-
-    def remove_data(self):
-        """Removes all data from the database"""
-        try:
-            conn = sqlite3.connect("weather.sqlite")
-            c = conn.cursor()
-            c.execute("DELETE FROM weather")
-            conn.commit()
-            c.execute("DROP TABLE weather")
-            conn.commit()
-        except Exception as e:
-            print("Error removing data.", e)
-        conn.close()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Close the connection
+        """
+        self.conn.close()
