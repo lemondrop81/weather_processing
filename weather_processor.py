d194b47804a5b82157f045b5b713d7111fbfa8a0
lemondrop81 <hacker814@hotmail.com>
2022-04-11 09:00:04 -0500

    Create weather processor class and file

---
+++ weather_processor.py
@@ -0,0 +1,8 @@
+"""
+    Weather processing app
+    April 11, 2022
+    Description: A simple program to add user interface.
+"""
+
+class WeatherProcessor():
+    """Contains the code for the user interface"""
