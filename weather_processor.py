cbe084223151bab848c26184db6697cb4ada66ef
lemondrop81 <hacker814@hotmail.com>
2022-04-11 09:18:46 -0500

    Update prompts for boxplot year

--- weather_processor.py
+++ weather_processor.py
@@ -10,4 +10,10 @@ from plot_operations import PlotOperations
 class WeatherProcessor():
     """Contains the code for the user interface"""
     userSelection = input("Fetch all available weather data, update existing, or skip? ([F]ull/[U]pdate/[S]kip):")
-    print(userSelection)
+    
+    if userSelection == 'F' or userSelection == 'f':
+        DBOperations.purge_data()
+        WeatherScraper.get_data()
+    
+    initialYear = input("Enter from year [YYYY]: ")
+    finalYear = input("Enter to year [YYYY]: ")
