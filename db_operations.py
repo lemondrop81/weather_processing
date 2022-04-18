f2a39ff62741f8d5fce42dc7527f574d707340a2
lemondrop81 <hacker814@hotmail.com>
2022-04-18 08:24:42 -0500

    Update db_operations for pep8 complience

--- db_operations.py
+++ db_operations.py
@@ -1,14 +1,11 @@
-from asyncio.windows_events import NULL
-from email.policy import default
-import sqlite3
-import dbcm
-import plot_operations
-
 """
     Weather processing app
     March 30, 2022
     Description: A simple program to add to the database.
 """
+from asyncio.windows_events import NULL
+from email.policy import default
+import dbcm
 
 class DBOperations():
     """Contains database operations"""
@@ -22,9 +19,9 @@ class DBOperations():
         try:
             with dbcm.DBCM("weather.sqlite") as conn:
                 print("Opened database successfully.")
-                try:    
-                    c = conn.cursor()
-                    c.execute("""create table IF NOT EXISTS weather 
+                try:
+                    connection = conn.cursor()
+                    connection.execute("""create table IF NOT EXISTS weather
                                     (id integer primary key autoincrement,
                                     sample_date text key not null,
                                     location text,
@@ -33,68 +30,69 @@ class DBOperations():
                                     avg_temp real);""")
                     conn.commit()
                     print("Table created successfully.")
-                except Exception as e:
-                    print("Error creating table:", e)
-        except Exception as e:
-            print("Error opening DB:", e)
+                except Exception as exception:
+                    print("Error creating table:", exception)
+        except Exception as exception:
+            print("Error opening DB:", exception)
 
     def save_data(self, weather):
         """Save the data to the database"""
         try:
             with dbcm.DBCM("weather.sqlite") as conn:
-                c = conn.cursor()
-                sql = """insert OR REPLACE into weather (sample_date,location,min_temp,max_temp,avg_temp)
+                connection = conn.cursor()
+                sql = """insert OR REPLACE into weather
+                        (sample_date,location,min_temp,max_temp,avg_temp)
                         values (?,?,?,?,?)"""
-                for k, v in weather.items():
-                    data = (k, 'Winnipeg, MB', v['Min'], v['Max'], v['Mean'])
-                    c.execute(sql, data)
+                for k, value in weather.items():
+                    data = (k, 'Winnipeg, MB', value['Min'], value['Max'], value['Mean'])
+                    connection.execute(sql, data)
                     print(data)
                 conn.commit()
                 print("Added sample successfully.")
-        except Exception as e:
-            print("Error inserting sample.", e)
+        except Exception as exception:
+            print("Error inserting sample.", exception)
 
 
     def purge_data(self):
         """Purges all data from database"""
         try:
             with dbcm.DBCM("weather.sqlite") as conn:
-                c = conn.cursor()
-                c = conn.cursor()
-                c.execute("DELETE FROM weather")
+                connection = conn.cursor()
+                connection = conn.cursor()
+                connection.execute("DELETE FROM weather")
                 conn.commit()
-                c.execute("DROP TABLE weather")
+                connection.execute("DROP TABLE weather")
                 conn.commit()
                 print("Successfully removed data from database")
-        except Exception as e:
-            print("Error removing data.", e)
+        except Exception as exception:
+            print("Error removing data.", exception)
 
     def fetch_data(self, inital=default, final=default, year=default, month=default):
-       """returns the data from database"""
-       try:
+        """returns the data from database"""
+        try:
            #Get the latest date from the database
-           if inital == NULL and final == NULL and year == NULL and month == NULL:
+            if inital == NULL and final == NULL and year == NULL and month == NULL:
                 with dbcm.DBCM("weather.sqlite") as conn:
-                    c = conn.cursor()
-                    c.execute("SELECT MAX(sample_date) FROM weather")
-                    row = c.fetchone()
+                    connection = conn.cursor()
+                    connection.execute("SELECT MAX(sample_date) FROM weather")
+                    row = connection.fetchone()
                     return row
-            # Get the weather data for the boxplot 
-           if inital != NULL and final != NULL:
+            # Get the weather data for the boxplot
+            if inital != NULL and final != NULL:
                 with dbcm.DBCM("weather.sqlite") as conn:
-                    c = conn.cursor()
-                    c.execute(f"select * from weather WHERE sample_date BETWEEN {inital} AND {final}" )
-                    rows = c.fetchall()
+                    connection = conn.cursor()
+                    connection.execute(f"select * from weather WHERE sample_date BETWEEN {inital}AND{final}")
+                    rows = connection.fetchall()
                     return rows
             # Get the weather data for the lineplot
-           if year != NULL and month != NULL:
+            if year != NULL and month != NULL:
                 with dbcm.DBCM("weather.sqlite") as conn:
-                    c = conn.cursor()
+                    connection = conn.cursor()
                     test = f"select * from weather WHERE sample_date LIKE '{year}-{month}%'"
-                    c.execute(test)
-                    rows = c.fetchall()
+                    connection.execute(test)
+                    rows = connection.fetchall()
                     return rows
 
-       except Exception as e:
-           print("Error fetching data.", e)
+        except Exception as exception:
+            print("Error fetching data.", exception)
         
