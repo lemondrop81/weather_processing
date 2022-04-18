72b407784124f6ed96630d5322612e3e2e97af7a
lemondrop81 <hacker814@hotmail.com>
2022-04-18 10:01:01 -0500

    Weather processor pep8 compliant

--- weather_processor.py
+++ weather_processor.py
@@ -1,37 +1,35 @@
-from asyncio.windows_events import NULL
-from scrape_weather import WeatherScraper
-from db_operations import DBOperations
-from plot_operations import PlotOperations
 """
     Weather processing app
     April 11, 2022
     Description: A simple program to add user interface.
 """
+from asyncio.windows_events import NULL
+from scrape_weather import WeatherScraper
+from db_operations import DBOperations
+from plot_operations import PlotOperations
 
 class WeatherProcessor():
     """Contains the code for the user interface"""
     def __init__(self):
         """Constructor"""
-        userSelection = input("Fetch all available weather data, update existing, or skip? ([F]ull/[U]pdate/[S]kip): ")
-        
-        if userSelection == 'F' or userSelection == 'f':
-            removeData = DBOperations()
-            removeData.purge_data()
+        user_selection = input("""Fetch all available weather data, update existing, or skip? ([F]ull/[U]pdate/[S]kip): """)
+        if user_selection in ('F', 'f'):
+            remove_data = DBOperations()
+            remove_data.purge_data()
 
             myparser = WeatherScraper()
             myparser.get_data(0)
 
-        if userSelection == 'U' or userSelection == 'u':
+        if user_selection in ('U', 'u'):
             weather = DBOperations.fetch_data(self,  NULL, NULL,  NULL, NULL)
 
             myparser = WeatherScraper()
             myparser.get_data(weather)
-        
-        initialYear = input("Enter from year [YYYY]: ")
-        finalYear = input("Enter to year [YYYY]: ")
+        initial_year = input("Enter from year [YYYY]: ")
+        final_year = input("Enter to year [YYYY]: ")
 
-        weather = DBOperations.fetch_data(self, initialYear, finalYear)
-        PlotOperations.boxplot(self, weather, initialYear, finalYear)
+        weather = DBOperations.fetch_data(self, initial_year, final_year)
+        PlotOperations.boxplot(self, weather, initial_year, final_year)
 
         year = input("Enter year [YYYY]: ")
         month = input("Enter month [MM]: ")
@@ -40,4 +38,4 @@ class WeatherProcessor():
         PlotOperations.lineplot(self, lineplot)
         space = input("")
 
-myparser = WeatherProcessor()
+test = WeatherProcessor()
