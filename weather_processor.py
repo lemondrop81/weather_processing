c7fb779609b8ed91cb6cc4c91bb0ecd5c9702911
lemondrop81 <hacker814@hotmail.com>
2022-04-18 11:03:38 -0500

    Add error handling to weather processor

--- weather_processor.py
+++ weather_processor.py
@@ -12,27 +12,30 @@ class WeatherProcessor():
     """Contains the code for the user interface"""
     def __init__(self):
         """Constructor"""
-        user_selection = input("""Fetch all available weather data, update existing, or skip? ([F]ull/[U]pdate/[S]kip): """)
-        if user_selection in ('F', 'f'):
-            remove_data = DBOperations()
-            remove_data.purge_data()
+        try:
+            user_selection = input("""Fetch all available weather data, update existing, or skip? ([F]ull/[U]pdate/[S]kip): """)
+            if user_selection in ('F', 'f'):
+                remove_data = DBOperations()
+                remove_data.purge_data()
 
-            myparser = WeatherScraper()
-            myparser.get_data(0)
+                myparser = WeatherScraper()
+                myparser.get_data(0)
 
-        if user_selection in ('U', 'u'):
-            weather = DBOperations.fetch_data(self,  NULL, NULL,  NULL, NULL)
+            if user_selection in ('U', 'u'):
+                weather = DBOperations.fetch_data(self,  NULL, NULL,  NULL, NULL)
 
-            myparser = WeatherScraper()
-            myparser.get_data(weather)
-        initial_year = input("Enter from year [YYYY]: ")
-        final_year = input("Enter to year [YYYY]: ")
+                myparser = WeatherScraper()
+                myparser.get_data(weather)
+            initial_year = input("Enter from year [YYYY]: ")
+            final_year = input("Enter to year [YYYY]: ")
 
-        weather = DBOperations.fetch_data(self, initial_year, final_year)
-        PlotOperations.boxplot(self, weather, initial_year, final_year)
+            weather = DBOperations.fetch_data(self, initial_year, final_year)
+            PlotOperations.boxplot(self, weather, initial_year, final_year)
 
-        year = input("Enter year [YYYY]: ")
-        month = input("Enter month [MM]: ")
+            year = input("Enter year [YYYY]: ")
+            month = input("Enter month [MM]: ")
+        except ValueError:
+            print("did not enter number")
 
         lineplot = DBOperations.fetch_data(self, NULL, NULL, year, month)
         PlotOperations.lineplot(self, lineplot)
