1029fa350ccb3d6e6ecc02bfa05a8d0a3aaa36e0
lemondrop81 <hacker814@hotmail.com>
2022-04-22 07:49:49 -0500

    Update plot_operations for pep8

--- plot_operations.py
+++ plot_operations.py
@@ -3,14 +3,15 @@
     April 8, 2022
     Description: A simple program to add to the database.
 """
-import matplotlib.pyplot as plt
 import logging
+import matplotlib.pyplot as plt
 
 class PlotOperations():
     """Contains the code for plotting"""
 
     def __init__(self):
         """Constructor"""
+        self.logger = logging.getLogger(__name__)
 
     def boxplot(self, weather, initial_year, final_year):
         """create the box plot"""
@@ -25,18 +26,18 @@ class PlotOperations():
                     box_weather.append(int(month[1]))
                     mean_temp.append(current_temp[5])
             except ValueError as error:
-                self.logger.error('PlotOperations:boxplot:for', error)
+                self.logger.error('PlotOperations:boxplot:for')
             groups = [[] for i in range(max(box_weather))]
             [groups[box_weather[i]-1].append(mean_temp[i]) for i in range(len(mean_temp))]
             fig = plt.figure()
-            ax  = fig.add_subplot(111)   # define the axis
-            ax.boxplot(groups)
-            ax.set_title(f'Monthly Temperature Distribution for: {initial_year} to {final_year}')
-            ax.set_xlabel('Month')
-            ax.set_ylabel('Temperature (Celsius)')
+            axis  = fig.add_subplot(111)   # define the axis
+            axis.boxplot(groups)
+            axis.set_title(f'Monthly Temperature Distribution for: {initial_year} to {final_year}')
+            axis.set_xlabel('Month')
+            axis.set_ylabel('Temperature (Celsius)')
             plt.show()
         except Exception as error:
-            self.logger.INFO('PlotOperations:boxplot', error)
+            self.logger.INFO(error)
 
     def lineplot(self, weather):
         """create the line plot"""
@@ -52,10 +53,10 @@ class PlotOperations():
             except ValueError as error:
                 self.logger.error('PlotOperations:lineplot')
             fig = plt.figure()
-            ax  = fig.add_subplot(111)   # define the axis
-            ax.set_title('Daily Avg Temperatures')
-            ax.set_xlabel('Day of Month')
-            ax.set_ylabel('Average Daily Temp')
+            axis  = fig.add_subplot(111)   # define the axis
+            axis.set_title('Daily Avg Temperatures')
+            axis.set_xlabel('Day of Month')
+            axis.set_ylabel('Average Daily Temp')
 
             # automaticall set font and rotation for date tick labels
             plt.gcf().autofmt_xdate()
@@ -63,4 +64,4 @@ class PlotOperations():
             plt.plot(date, mean_temp)
             plt.show()
         except Exception as error:
-            self.logger.INFO('PlotOperations:lineplot', error)
+            self.logger.INFO(error)
